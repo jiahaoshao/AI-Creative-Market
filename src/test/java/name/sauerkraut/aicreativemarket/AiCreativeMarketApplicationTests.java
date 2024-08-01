f066938f74408cbf9f910dfe4d583e1d67e3fe45
fangyi <2370145097@qq.com>
2024-08-01 17:37:40 +0800

    Initial commit

---
+++ src/test/java/name/sauerkraut/aicreativemarket/AiCreativeMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package name.sauerkraut.aicreativemarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiCreativeMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
