7d733fe2380f0a3ebfbd7382f42ab91711079101
fangyi <2370145097@qq.com>
2024-08-02 19:09:15 +0800

    8.2

--- src/main/java/name/sauerkraut/aicreativemarket/controller/LoginController.java
+++ src/main/java/name/sauerkraut/aicreativemarket/controller/LoginController.java
@@ -3,13 +3,8 @@ package name.sauerkraut.aicreativemarket.controller;
 import name.sauerkraut.aicreativemarket.bean.UserBean;
 import name.sauerkraut.aicreativemarket.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Objects;
 
 @Controller
 public class LoginController {
@@ -23,11 +18,11 @@ public class LoginController {
     }
 
     //登录逻辑
-    @RequestMapping(value = "/loginIn",method = RequestMethod.POST)
+    @RequestMapping(value = "/home",method = RequestMethod.POST)
     public String loginIn(String email, String password) {
         UserBean userBean = userService.loginIn(email, password);
         if(userBean != null) {
-            return "success";
+            return "home";
         }else{
             return "error";
         }
